da1b8491ebcecf4023fc4ec8ee995d28da7945b4
Maks <zabavnikov.maxim@gmail.com>
2026-06-11 12:03:10 +0300

    Add company views

--- apps/company/views.py
+++ apps/company/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Cluster, Contractor, Employee, Store
+from .serializers import (
+    ClusterSerializer,
+    ContractorSerializer,
+    EmployeeSerializer,
+    StoreSerializer
+)
 
-# Create your views here.
+class EmployeeViewSet(ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class ClusterViewSet(ModelViewSet):
+    queryset = Cluster.objects.all()
+    serializer_class = ClusterSerializer
+
+class ContractorViewSet(ModelViewSet):
+    queryset = Contractor.objects.all()
+    serializer_class = ContractorSerializer
+
+class StoreViewSet(ModelViewSet):
+    queryset = Store.objects.all()
+    serializer_class = StoreSerializer
