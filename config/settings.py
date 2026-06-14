9f87e2fb053f8787ceab8a5812b941a69137bd63
Maks <zabavnikov.maxim@gmail.com>
2026-06-14 16:22:14 +0300

    Add OpenAPI documentation

--- config/settings.py
+++ config/settings.py
@@ -38,10 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    "drf_spectacular",
     'apps.company',
     'apps.inspections',
     'apps.tickets',
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -124,3 +129,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "StoreCheck API",
+    "DESCRIPTION": "API for store inspections, checklist reports, and maintenance tickets.",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+}
